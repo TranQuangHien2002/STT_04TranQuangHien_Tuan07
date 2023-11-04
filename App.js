49223534b4c2744cf2398196a1e3ef71d813b0a1
TranQuangHien <tqhien27102002@gmail.com>
2023-11-04 23:13:36 +0700

    get API

--- App.js
+++ App.js
@@ -1,3 +1,31 @@
-import API_Screen_01 from './component/API_Screen_01'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import screen1 from "./screens/Screen1";
+import screen2 from "./screens/Screen2";
+import screen3 from "./screens/Screen3"
 
-export default API_Screen_01
+const Stack = createStackNavigator()
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+       
+        <Stack.Screen
+          name="screen1"
+          component={screen1}
+          options={{headerShown:false}}
+        />
+         <Stack.Screen
+          name="screen2"
+          component={screen2}
+          options={{ headerShown: false }}
+        />
+            <Stack.Screen
+          name="screen3"
+          component={screen3}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
